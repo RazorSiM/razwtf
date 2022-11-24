cad6744199db1b6a0e8264b84ea7e85d2641aeeb
Raz <me@raz.works>
2022-11-24 16:39:02 +0100

    fix text lint

--- components/DocumentDrivenEmpty.vue
+++ components/DocumentDrivenEmpty.vue
@@ -1,3 +1,5 @@
 <template>
-  <h1 class="w-full text-center min-h-40 mt-30">Quì non c'è niente!</h1>
+  <h1 class="w-full text-center min-h-40 mt-30">
+    This page is empty!
+  </h1>
 </template>
--- components/DocumentDrivenNotFound.vue
+++ components/DocumentDrivenNotFound.vue
@@ -1,3 +1,5 @@
 <template>
-  <h1 class="w-full text-center min-h-40 mt-30">Pagina non trovata!</h1>
+  <h1 class="w-full text-center min-h-40 mt-30">
+    Page not found!
+  </h1>
 </template>
