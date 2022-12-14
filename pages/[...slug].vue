15fc28cefc970617ccfdf16aca2da1fd1a0fe779
Raz <me@raz.works>
2022-12-14 20:49:49 +0100

    fix base pages content background

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -9,7 +9,7 @@ if (!page.value && process.server) {
 </script>
 
 <template>
-  <div class="max-w-2xl mx-auto">
+  <div class="max-w-2xl mx-auto bg-base rounded-xl p-4">
     <ContentRenderer v-if="page" :key="page._id" :value="page">
       <template #empty="{ value }">
         <DocumentDrivenEmpty :value="value" />
