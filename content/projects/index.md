f065319a9198ff8c00e035893f6aa4c21bf0c8f7
Raz <me@raz.works>
2022-12-14 19:10:57 +0100

    added content to the projects/index page

---
+++ content/projects/index.md
@@ -0,0 +1,1 @@
+In this page you'll find **some** of my projects. Most of the time I work on private projects (for companies, mostly) and I can't share them, but I'll try to keep this page updated with the ones I can.
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -2,6 +2,7 @@
 definePageMeta({
   layout: 'default',
 })
+const { page } = useContent()
 const elementsPerPage = 6
 const currentPage = 1
 const projects = await queryContent('projects').limit(elementsPerPage).skip(elementsPerPage * (currentPage - 1)).sort({ date: -1 }).find()
@@ -13,8 +14,11 @@ const nextPage = nextProjects.length === 1 ? `/projects/_${currentPage + 1}` : n
 
 <template>
   <div class="max-w-4xl mx-auto">
+    <ContentRenderer :key="page._id" :value="page" />
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-      <ContentCard v-for="project in projects" :key="project._path" :title="project.title ?? ''" :image="project.hero" :excerpt="project.excerpt ?? ''" :url="project._path ?? ''" :date="project.date" :timetoread="project.readingTime.text" />
+      <template v-for="project in projects">
+        <ContentCard v-if="project._path !== '/projects'" :key="project._path" :title="project.title ?? ''" :image="project.hero" :excerpt="project.excerpt ?? ''" :url="project._path ?? ''" :date="project.date" :timetoread="project.readingTime.text" />
+      </template>
     </div>
     <Pagination :previous-page="previousPage" :next-page="nextPage" class="mt-10 justify-center" />
   </div>
