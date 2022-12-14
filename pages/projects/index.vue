20a186b9df7e9906d120a861e14505b9f092f7fc
Raz <me@raz.works>
2022-12-14 19:29:51 +0100

    fix

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -2,7 +2,6 @@
 definePageMeta({
   layout: 'default',
 })
-const { page } = useContent()
 const elementsPerPage = 6
 const currentPage = 1
 const projects = await queryContent('projects').limit(elementsPerPage).skip(elementsPerPage * (currentPage - 1)).sort({ date: -1 }).find()
@@ -14,7 +13,9 @@ const nextPage = nextProjects.length === 1 ? `/projects/_${currentPage + 1}` : n
 
 <template>
   <div class="max-w-4xl mx-auto">
-    <ContentRenderer :key="page._id" :value="page" />
+    <p class="mb-10 max-w-lg mx-auto">
+      In this page you'll find of my projects. Most of the time I work on private projects (for companies, mostly) and I can't share them, but I'll try to keep this page updated with the ones I can.
+    </p>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
       <template v-for="project in projects">
         <ContentCard v-if="project._path !== '/projects'" :key="project._path" :title="project.title ?? ''" :image="project.hero" :excerpt="project.excerpt ?? ''" :url="project._path ?? ''" :date="project.date" :timetoread="project.readingTime.text" />
