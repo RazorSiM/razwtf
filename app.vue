24a78fc68ea6b2a3649b3dfe202309f961d4cb2d
Raz <me@raz.wtf>
2023-09-13 00:29:37 +0200

    improve SEO

--- app.vue
+++ app.vue
@@ -1,5 +1,43 @@
 <script lang="ts" setup>
 provideLightbox()
+
+const description = 'Raz.WTF - Web Designer & Developer. I build things on the web, like this website'
+const title = 'Raz - Simone Colabufalo'
+const socialTitle = 'Raz.WTF'
+const image = '/socialShare.jpg'
+const imageAlt = 'Raz Logo'
+const twitterName = '@Razor_SiM'
+useSeoMeta({
+  title,
+  description,
+  twitterCard: 'summary_large_image',
+  twitterSite: twitterName,
+  twitterCreator: twitterName,
+  twitterTitle: socialTitle,
+  twitterDescription: description,
+  twitterImage: image,
+  twitterImageAlt: imageAlt,
+  ogTitle: socialTitle,
+  ogDescription: description,
+  ogImage: image,
+  ogImageAlt: imageAlt,
+  ogSiteName: socialTitle,
+  ogType: 'website',
+  ogLocale: 'en_US',
+  ogUrl: 'https://raz.wtf',
+})
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png',
+    },
+  ],
+})
 </script>
 
 <template>
